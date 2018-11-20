a9888d10caa491a581b3c94647ad7befe0ca4bde
Anna Burley <anna.burley3@gmail.com>
2018-11-20 00:01:03 +0000

    added arrows as js elements and rewrote function to choose template

--- js/main.js
+++ js/main.js
@@ -1,22 +1,52 @@
-const templatesArray = [`intro`, `greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`, `modal-error`, `modal-confirm`];
 
-function chooseTemplate(templateNumber) {
-  const templateUsed = document.querySelector(`#${templatesArray[templateNumber]}`);
-  const clone = document.importNode(templateUsed.content, true);
-  document.body.querySelector(`#main`).appendChild(clone);
+const templateArray = document.querySelectorAll(`template`);
+const mainElement = document.querySelector(`#main`);
+
+const chooseTemplate = (currentTemplateNumber) => {
+  const shadow = document.createElement(`div`);
+  const content = templateArray[currentTemplateNumber].content.cloneNode(true);
+  shadow.appendChild(content);
+  mainElement.appendChild(shadow);
+  return shadow.cloneNode(true);
 }
-
 
 chooseTemplate(2);
 
-document.addEventListener(`keydown`, screen);
+const arrowWrapper = document.createElement(`div`);
+arrowWrapper.className = `arrows__wrap`;
+document.body.appendChild(arrowWrapper);
 
-function screen(e) {
-  const keyCode = e.keyCode;
-  if (keyCode === 39) {
-    chooseTemplate(templateNumber + 1);
-  } else if ((keyCode === 37)) {
-    chooseTemplate(templateNumber - 1);
-  }
-}
+
+const buttonSwitchPrev = document.createElement(`button`);
+const buttonSwitchNext = document.createElement(`button`);
+arrowWrapper.appendChild(buttonSwitchPrev);
+arrowWrapper.appendChild(buttonSwitchNext);
+
+const arrowLeft = document.createTextNode(`<-`);
+const arrowRight = document.createTextNode(`->`);
+buttonSwitchPrev.appendChild(arrowLeft);
+buttonSwitchNext.appendChild(arrowRight);
+buttonSwitchNext.className = `arrows__btn`;
+buttonSwitchPrev.className = `arrows__btn`;
+
+//
+// function chooseTemplate(templateNumber) {
+//   const templateUsed = document.querySelector(`#${templatesArray[templateNumber]}`);
+//   const clone = document.importNode(templateUsed.content, true);
+//   document.body.querySelector(`#main`).appendChild(clone);
+// }
+//
+//
+// chooseTemplate(2);
+//
+// document.addEventListener(`keydown`, screen);
+//
+// function screen(e) {
+//   const keyCode = e.key;
+//   if (keyCode === 39) {
+//     chooseTemplate(templateNumber + 1);
+//   } else if ((keyCode === 37)) {
+//     chooseTemplate(templateNumber - 1);
+//   }
+// }
 
